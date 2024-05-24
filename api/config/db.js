fd234cc9f30f1e0f2fb552bec3840766eff2cea7
vivekpatil2001 <vivekshejole143@gmail.com>
2024-05-24 15:58:43 +0530

    connected mongodb uri

--- api/config/db.js
+++ api/config/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
+     useNewUrlParser: true,
       useUnifiedTopology: true
     });
     console.log('MongoDB connected...');
